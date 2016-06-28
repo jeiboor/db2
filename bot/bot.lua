37f89dd79505aa07965d4268e97852cdc2e98710
MR_saf <jeiboor@users.noreply.github.com>
2016-06-28 11:51:49 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,8 +256,8 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {231302652},
-    admin_users = {},
+    sudo_users = {187161700},
+    admin_users = {187161700},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
